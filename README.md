620c7a71759078c6cc871b8eda7ac5442694bd7f
Rijk van Zanten <rijkvanzanten@me.com>
2016-01-10 20:31:14 +0100

    First commit
    Clean install. klaar voor de start af.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Serverside Scripting Eindopdracht
+
+## Opdracht
+
+> Maak een foto-deel-applicatie met Node.js waar je op kunt inloggen en foto’s kunt uploaden als je ingelogd bent. Andere gebruikers die niet hoeven in te loggen moeten commentaar bij een foto kunnen plaatsen.
+
+## Installatie
+
+Download de applicatie en installeer met `npm install`. Voor meer informatie over hoe je een app zoals deze maakt; zie m'n [documentatie](http://rijkvanzanten.nl/sss-doc).
+
+## Packages
+
+- Express
+- EJS
+- Body-parser
+- Express-myconnection
+- Express-session
+- Multer
+- Mysql
+
---
+++ app.js
@@ -0,0 +1,52 @@
+// Require the dependencies
+// --------------------------------------------------------------------
+var express 	 = require('express'),
+	session		 = require('express-session'),
+	myConnection = require('express-myconnection'),
+	mysql 		 = require('mysql'),
+	bodyParser   = require('body-parser');
+
+
+
+// Setup the packages
+// --------------------------------------------------------------------
+// Express
+var app = express();
+app.use(express.static('public'));
+
+// EJS
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Sessions
+app.use(session({
+	secret: "P85dvHsxWp0XFD8kfh3VwkKcj3cvs2GT",
+	saveUninitialized: true,
+	resave: false
+}));
+
+// MySQL
+app.use(myConnection(mysql, {
+	host: 'localhost',
+	user: 'student',
+	password: 'serverSide',
+	port: 3306,
+	database: 'eindopdracht'
+}, 'single'));
+
+
+// Routes & Routers
+// --------------------------------------------------------------------
+var indexRouter = require('./routes/index.js');
+app.use('/', indexRouter);
+
+
+// Start server
+// --------------------------------------------------------------------
+app.listen(3000, function() {
+	console.log('App started. Listening on default port (3000)');
+});
